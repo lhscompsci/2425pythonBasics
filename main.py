43a0a236d00e7b1a1af8b167235f913095287318
Sam Black <samblack@gmail.com>
2024-10-08 13:14:21 -0500

    Pushing Notes

---
+++ main.py
@@ -0,0 +1,63 @@
+print("CompSci Rocks!", end = " ") #the end tag keeps the output on the same line
+print("I love compsci!")
+
+"""
+everything here is a comment
+no matter how many lines there are
+"""
+# Escape Sequences = always start with a \
+print("\"Howdy\" \tpar\ndner\\!")
+"""
+#variables
+grade = 86
+student = "Bob"
+theEnd = False
+grade = 92.3
+aList = ["Jane",95,True]    # list -- editable
+group = ("Jane",95,True)    # tuple -- unchangeable
+name = "Ben"
+print("the questioner is", name)
+numString = "56"
+num = int(numString)  #casting from string to int
+print(4 + num)
+name3 = input("What is your name?")
+faveNum = int(input("What is your favorite number?"))
+change = float(input("How much change is in your pocket?"))
+print("your name is", name3)
+print("your favorite number is", faveNum)
+print("you have this amount in your pocket", change)
+"""
+
+# Math lesson
+numOne = 15
+numTwo = 37
+numThree = 42
+total = numOne + numTwo + numThree
+print(total)
+product = numOne * numTwo * numThree
+print(product)
+realDivision = numOne / numTwo # real division
+print(realDivision)
+intDivision = numTwo // numOne  # integer (floor) division
+print(intDivision)
+remainder = numTwo % numOne     # modulus (remainder)
+print(remainder)
+powers = numOne ** 2    # exponents
+print(powers)
+
+# Shortcuts
+numOne = numOne + 10
+numOne += 5
+print(numOne)
+
+num = 11
+num %= 2
+print(num)
+num = 55
+num //= 2
+print(num)
+num = 55
+num /= 2
+print(num)
+
+
